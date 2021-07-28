a784be683359d775de7b67b2655bccdc5c49f6d0
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2021-07-29 05:42:49 +0700

    add task 2

--- my third cpp/main.cpp
+++ my third cpp/main.cpp
@@ -11,29 +11,39 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     
-    int n, t1 = 0, t2 = 1, nextTerm = 0;
+//    int n, t1 = 0, t2 = 1, nextTerm = 0;
+//
+//    cout << "Enter the number of terms: ";
+//    cin >> n;
+//
+//    cout << "Fibonacci Series: ";
+//
+//    for (int i = 1; i <= n; ++i) {
+//        // Prints the first two terms.
+//        if(i == 1) {
+//            cout << t1 << ", ";
+//            continue;
+//        }
+//        if(i == 2) {
+//            cout << t2 << ", ";
+//            continue;
+//        }
+//        nextTerm = t1 + t2;
+//        t1 = t2;
+//        t2 = nextTerm;
+//
+//        cout << nextTerm << ", ";
+//    }
+    
+    int n, result = 1;
 
-    cout << "Enter the number of terms: ";
-    cin >> n;
+        cout << "Enter a non-negative number: ";
+        cin >> n;
 
-    cout << "Fibonacci Series: ";
-
-    for (int i = 1; i <= n; ++i) {
-        // Prints the first two terms.
-        if(i == 1) {
-            cout << t1 << ", ";
-            continue;
+        for (int i = 1; i <= n; i++){
+            result *= i;
         }
-        if(i == 2) {
-            cout << t2 << ", ";
-            continue;
-        }
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
-        
-        cout << nextTerm << ", ";
-    }
+        cout << "Factorial of " << n << " = " << result << endl;
     
     return 0;
 }
